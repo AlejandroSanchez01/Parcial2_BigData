1ef7845b59373eca7b24ec7d8e49a59a220c4df9
Ubuntu <Herlendy.sanchez11@gmail.com>
2023-04-23 13:18:03 +0000

    Archivos

---
+++ test_noticias.py
@@ -0,0 +1,13 @@
+from noticias import download_page
+from unittest import mock
+
+
+def test_download_page(mocker):
+
+    content = "<html><head><title>Example</title></head> \
+    <body><h1>Example</h1></body></html>"
+    mocker.patch('urllib.request.urlopen', return_value=mock.Mock(
+        read=lambda: content.encode()))
+    respuesta = download_page(
+        'https://www.eltiempo.com', 'bucket-news23', 'elTiempo-')
+    assert respuesta == content
